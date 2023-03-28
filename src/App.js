1bde795767f9ad3164820438e642150d5477574d
Mahdi Jenhani <mahdijenhani00@gmail.com>
2023-03-28 10:13:51 +0100

    Add task submit

--- src/App.js
+++ src/App.js
@@ -24,6 +24,12 @@ function App() {
         reminder: false,
     }
 ])
+  //Add Task
+  const addTask = (task) => {
+    const id= Math.floor(Math.random() * 10000)+1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
 
   // Delete Task
   const deleteTask = (id) => {
@@ -43,7 +49,7 @@ function App() {
   return (
     <div className="container">
       <Header/>
-      <AddTask/>
+      <AddTask onAdd={addTask}/>
       {tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
   );
